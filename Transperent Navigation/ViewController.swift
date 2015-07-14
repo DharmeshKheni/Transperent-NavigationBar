098ed839bb4f212e4e463e3046ef3dc7122ba052
Dharmesh <khenid21@gmail.com>
2015-07-14 12:20:16 +0530

    Transparent NavigationBar
    Transparent NavigationBar

---
+++ Transperent Navigation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Transperent Navigation
+//
+//  Created by Anil on 14/07/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        self.navigationController!.navigationBar.translucent = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
